022931b43d32a2ab6fe1b668a1c57d1a8abcf7f8
Krzysztof Mazur <krzysztof.mazur@mds.com.pl>
2017-10-17 16:33:08 +0200

    last fixes to configuration

---
+++ app/src/main/java/pl/com/AppApplication.java
@@ -0,0 +1,14 @@
+package pl.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
