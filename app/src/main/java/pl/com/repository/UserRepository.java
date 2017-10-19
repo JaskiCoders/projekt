0d9c1f9d5ad1e93d22015621662a46df079c3151
Krzysztof Mazur <krzysztof.mazur@mds.com.pl>
2017-10-19 19:39:57 +0200

    Finding users by role

--- app/src/main/java/pl/com/repository/UserRepository.java
+++ app/src/main/java/pl/com/repository/UserRepository.java
@@ -2,10 +2,14 @@ package pl.com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.com.model.Role;
 import pl.com.model.User;
+
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     User findByLogin(String login);
+    Set<User> findByRole(Role role);
 }
